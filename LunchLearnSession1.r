b11a2820307a7e82895c1053b6be15e7ebb8c3fe
Cary Hernandez <chernandez@quantcast.com>
2016-04-13 14:12:05 -0700

    Update today

--- LunchLearnSession1.r
+++ LunchLearnSession1.r
@@ -2,42 +2,21 @@
 ### Session #1
 ### Chris Garnatz and Cary Hernandez
 
-getwd()
-# Prints out your working directory
-mean??
-setwd()
-# Changes your current working directory
-
 install.packages(packagename)
 # Coding command to install a package
 # Package - a combination of additional functions and features not in basic R
 
+library(packagename)
+# Loads the library that has been installed
 
 
-# Importing Data - still need to make testdata.csv file !!!!!!
+#### String/Variable Types ####
 
-Augmentdata <- read.csv(file="C:/Users/chernandez/Desktop/R Lunch and Learns/testdata.csv")
-# The CSV file is now assigned to a dataframe called Aug_data
-# To see the dataset type in the dataframe's name
-Augmentdata
-
-head(Augmentdata)
-# Prints out the first ten rows of the dataset
-
-str(Augmentdata)
-# Prints out the structure of the dataset and each column/variable type
-# Example - $ Ex.Imps is an integer and shows the some values of that column
 
 # Int - integer 
 # Num - numeric number so it can have decimals
 # Factor - categorical or character variable. ex.) Data Source "adtech", "delta", etc 
 # Logi - logical, Yes/No/NA or 1/0/NA
-
-
-Augmentdata$CPA
-# Access all CPA values in our dataset
-
-
 
 # Each column is treated like a vector - a list of values. Yet, not all vectors are in data.frames
 Budget_vector <- c(3000,5000,3400)
@@ -64,9 +43,10 @@ mean(dayofweek)
 
 
 ### D.I.Y. Make your own Vectors of numeric and character types!!!!!
+# Create a five number long vector and find the mean
 numeric_vector <- c()
 
-
+# Create a character vector with three different values and return it's length
 char_vec <- c("")
 
 
@@ -74,12 +54,61 @@ ls()
 # Displays all variables & data.frames available for use that you have created
 
 
+##### Go over Data.frame structure and visual #######
+
+
+
+# Importing Data - still need to make testdata.csv file !!!!!!
+
+Augmentdata <- read.csv(file="C:/Users/chernandez/Desktop/R Lunch and Learns/testdata.csv")
+# The CSV file is now assigned to a dataframe called Aug_data
+# To see the dataset type in the dataframe's name
+Augmentdata
+
+head(Augmentdata)
+# Prints out the first ten rows of the dataset
+
+str(Augmentdata)
+# Prints out the structure of the dataset and each column/variable type
+# Example - $ Ex.Imps is an integer and shows the some values of that column
+
+# Int - integer 
+# Num - numeric number so it can have decimals
+# Factor - categorical or character variable. ex.) Data Source "adtech", "delta", etc 
+# Logi - logical, Yes/No/NA or 1/0/NA
+
+summary(Augmentdata)
+
+Augmentdata$CPA
+# Access all CPA values in our dataset
+
+
+# Applying Functions onto columns in the example dataframe
+
+# tapply examples
+
+
+
+
+
+
+
+
+
+
 ### Give example of pulling in Measure Data downloaded. 
 # We will have to use the parameter skip. This tells how many rows to skip before beginning to read in data. 
-install.packages(gdata)
-library(gdata)
 
-Measure_Bonobos <- read.xls(file="C:/Users/chernandez/Downloads/_fp.event.Checkout+Confirmation.xls",skip=3)
+Measure_Bonobos <- read.csv(file="C:/Users/chernandez/Downloads/_fp.event.Checkout+Confirmation.csv",skip=3)
+
+
+
+
+
+
+
+
+
 
 
 
